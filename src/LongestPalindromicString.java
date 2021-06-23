8675d72d30a322dc67b42dc892ba168a109d6a9b
Nidhinraj P P <pnidhira@amazon.com>
2021-06-23 21:44:06 +0530

    Made changes longest palindromic substring program

--- src/LongestPalindromicString.java
+++ src/LongestPalindromicString.java
@@ -9,23 +9,21 @@
 public class LongestPalindromicString {
 
     public static boolean isPalindrome(String str) {
-        int midIndex = str.length() / 2;
-        String left = str.substring(0, midIndex);
-        String reverseStr = new StringBuilder(str).reverse().toString();
-        String right = reverseStr.substring(0, midIndex);
-        return left.equals(right);
+        StringBuilder sb = new StringBuilder(str);
+        return sb.reverse().toString().equals(str);
     }
 
     public static void printLongestPalindrome(String input) {
         String output = "";
-        for (int i = 0; i < input.length(); i++)
+        for (int i = 0; i < input.length(); i++) {
             for (int j = input.length(); j > i; j--) {
-                String subStr = input.substring(i, j);
-                if (isPalindrome(subStr) && subStr.length() > output.length()) {
-                    output = subStr;
+                String temp = input.substring(i, j);
+                if (isPalindrome(temp) && temp.length() > output.length()) {
+                    output = temp;
                 }
             }
-        System.out.println("The longest palindromic substring is : " + output);
+        }
+        System.out.println("Longest palindromic substring is : " + output);
     }
 
     public static void main(String[] args) {
