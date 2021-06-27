36158684c5c5983f1b8f2bcd1e337ca39da0134f
Nidhinraj P P <pnidhira@amazon.com>
2021-06-27 17:54:05 +0530

    Updated program for longest palindrome

--- src/LongestPossiblePalindrome.java
+++ src/LongestPossiblePalindrome.java
@@ -5,54 +5,42 @@ import java.util.HashMap;
  Write a program to find the length of longest possible palindrome from a given string.
 
  Input : abchellocab
- Output: 9, abclelcba
+ Output: 9, abclolcba
  *******************************************************************************/
 
 public class LongestPossiblePalindrome {
-    public static String longestPalindrome(String inputStr) {
-        HashMap<Character, Integer> map = getNumberOfOccurrences(inputStr.toCharArray());
-        StringBuilder sb = new StringBuilder();
-        Character mid = null;
-        for (char ch : map.keySet()) {
-            if (map.get(ch) == 1) {
-                mid = ch;
-                break;
-            }
-        }
-        for (char key : map.keySet()) {
-            while (map.get(key) / 2 > 1) {
-                sb.append(key);
-                map.put(key, map.get(key) - 2);
-            }
-        }
-        String left = sb.toString();
-        String right = sb.reverse().toString();
 
-        return left + mid + right;
+    public static HashMap<Character, Integer> numberOfOccurrences(String input) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (Character ch : input.toCharArray()) {
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) + 1);
+            } else {
+                map.put(ch, 1);
+            }
+        }
+        return map;
     }
 
-    public static HashMap<Character, Integer> getNumberOfOccurrences(char[] arr) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (char target : arr) {
-            for (char ch : arr) {
-                if (ch == target) {
-                    if (map.containsKey(target)) {
-                        map.put(target, map.get(target) + 1);
-                    } else {
-                        map.put(ch, 1);
-                    }
-                }
+    public static void createPalindrome(String input) {
+        HashMap<Character, Integer> map = numberOfOccurrences(input);
+        String mid = "";
+        StringBuilder sb = new StringBuilder();
+        for (Character key : map.keySet()) {
+            if (map.get(key) == 1) {
+                mid = String.valueOf(key);
             }
+            sb.append(String.valueOf(key).repeat(map.get(key) / 2));
         }
-        System.out.println("Hash Map : " + map.toString());
-        return map;
+        String palindrome = sb.toString();
+        palindrome += mid + sb.reverse().toString();
+        System.out.println("Longest possible palindrome is : " + palindrome);
+        System.out.println("Length og longest possible palindrome is : " + palindrome.length());
     }
 
     public static void main(String[] args) {
         String input = "abchellocab";
-        String output = longestPalindrome(input);
-        System.out.println("Longest possible palindrome is: " + output);
-        System.out.println("Length of longest possible palindrome is: " + output.length());
+        createPalindrome(input);
     }
 
 }
